4b1ef1ef4c279247eb12ef54711ad83beb8cd642
Fred George <fredgeorge@acm.org>
2026-03-27 14:52:03 +0100

    Fred: Cleaned up Gradle dependencies

--- engine/build.gradle.kts
+++ engine/build.gradle.kts
@@ -1,4 +1,11 @@
 dependencies {
+    implementation(libs.jackson.kotlin)
+    implementation(libs.jackson.databind)
+
+    // Optional: only keep these if you actually need them directly
+    implementation(libs.jackson.core)
+    implementation(libs.jackson.annotations)
+    implementation(libs.jackson.datatype)
     // Define your public API here. Example:
     // implementation("org.jetbrains.kotlin:kotlin-stdlib")
 }
--- tests/build.gradle.kts
+++ tests/build.gradle.kts
@@ -1,6 +1,14 @@
 dependencies {
     implementation(project(":engine"))
 
+    implementation(libs.jackson.kotlin)
+    implementation(libs.jackson.databind)
+
+    // Optional: only keep these if you actually need them directly
+    implementation(libs.jackson.core)
+    implementation(libs.jackson.annotations)
+    implementation(libs.jackson.datatype)
+
     testImplementation(platform("org.junit:junit-bom:6.0.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
