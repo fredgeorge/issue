b796f58547f2760657f003b164ef927bed4adbbb
Fred George <fredgeorge@acm.org>
2026-04-04 18:16:15 +0200

    Fred: Changed PrettyPrint to use StringBuilder for efficiency

--- engine/src/main/kotlin/com/nrkei/project/issue/PrettyPrint.kt
+++ engine/src/main/kotlin/com/nrkei/project/issue/PrettyPrint.kt
@@ -2,22 +2,22 @@ package com.nrkei.project.issue
 
 // Understands rendering IssueSet in a human-readable format
 internal class PrettyPrint(issueSet: IssueSet) : IssueVisitor {
-    private var result = ""
+    private val result = StringBuilder()
     init {
         issueSet.accept(this)
     }
 
-    override fun toString() = result
+    override fun toString() = result.toString()
 
     override fun preVisit(issueSet: IssueSet, types: List<IssueType<*>>) {
-        result += "IssueSet with Buckets for ${types.joinToString(", ") { it.javaClass.simpleName }}\n"
+        result.appendLine("IssueSet with Buckets for ${types.joinToString(", ") { it.javaClass.simpleName }}")
     }
 
     override fun preVisit(bundle: IssueSet.Bucket<*>, type: IssueType<*>) {
-        result += "\tBucket for ${type.javaClass.simpleName}:\n"
+        result.appendLine("\tBucket for ${type.javaClass.simpleName}:")
     }
 
     override fun visit(issue: Issue<*>, type: IssueType<*>) {
-        result += "\t\t${issue}\n"
+        result.appendLine("\t\t${issue}")
     }
 }
