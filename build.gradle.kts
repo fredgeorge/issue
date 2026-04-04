13c940f8bd86d4b72358478932a5b09a692aaf1d
Fred George <fredgeorge@acm.org>
2026-04-04 17:48:12 +0200

    Fred: Yes, even more tuning per Claude

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,13 +4,6 @@
  * Licensed under the MIT License; see LICENSE file in root.
  */
 
-plugins {
-    // Using aliases from the .toml
-    // IMPORTANT: Use all aliases or the false plugins below, not both!
-//    kotlin("jvm") version "2.3.20" apply false
-//    kotlin("plugin.serialization") version "2.3.20" apply false
-}
-
 allprojects {
     group = "com.nrkei.project"
     version = "1.1.1"
